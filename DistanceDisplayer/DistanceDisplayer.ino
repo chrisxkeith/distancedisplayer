58c094ffce4f3e29617c17accad3b12ad8ce50fd
Christopher Keith <chris.keith@gmail.com>
2020-04-19 17:04:22 -0700

    Try averaging a few samples.

--- DistanceDisplayer/DistanceDisplayer.ino
+++ DistanceDisplayer/DistanceDisplayer.ino
@@ -1,10 +1,10 @@
 
-# Please credit chris.keith@gmail.com .
+// Please credit chris.keith@gmail.com .
 
 const int trigPin = 6;
 const int echoPin = 7;
 
-const bool dbg = true;
+const bool dbg = false;
 void dbg_print(String s) {
   if (dbg) {
     String d = "debug: ";
@@ -13,17 +13,26 @@ void dbg_print(String s) {
   }
 }
 
-long calc_distance() {
-  int distanceInFeet = 200; 
-  do {      
+long sample() {
     digitalWrite(trigPin, LOW);   // Clear the trigPin
+    delayMicroseconds(4);
+    digitalWrite(trigPin, HIGH);  // Set the trigPin on HIGH state for n microseconds
     delayMicroseconds(20);
-    digitalWrite(trigPin, HIGH);  // Set the trigPin on HIGH state for 100 microseconds
-    delayMicroseconds(100);
     digitalWrite(trigPin, LOW);
     
     // Read the echoPin, return the sound wave travel time in microseconds
-    long duration = pulseIn(echoPin, HIGH);
+    return pulseIn(echoPin, HIGH);
+}
+
+long calc_distance() {
+  int distanceInFeet = 200;
+  do {
+    long sum_samples = 0;
+    const int MAX_SAMPLES = 5;
+    for (int n_samples = 0; n_samples < MAX_SAMPLES; n_samples++) {
+        sum_samples = sample();
+    }
+    long duration = sum_samples / MAX_SAMPLES;
     if (duration > 0) {
       String s = "duration(2) = ";
       s.concat(duration);
@@ -31,7 +40,7 @@ long calc_distance() {
       distanceInFeet = round((duration * 0.034 / 2) * 0.032);
     }
   } while (distanceInFeet > 16);
-  // Possibly In The Future: Figure out where the 103/104 distanceInFeet come from
+  // Possibly In The Future: Figure out where the 100+ distanceInFeet come from.
   return(distanceInFeet);
 }
 
