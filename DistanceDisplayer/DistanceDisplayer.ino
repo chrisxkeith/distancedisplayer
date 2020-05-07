d647ea4b1d1306edb4e779210552d1f66ce3feab
Christopher Keith <chris.keith@gmail.com>
2020-05-07 14:45:04 -0700

    First cut at getting laser distance sensor working.

--- DistanceDisplayer/DistanceDisplayer.ino
+++ DistanceDisplayer/DistanceDisplayer.ino
@@ -54,51 +54,50 @@ class EventSaver {
 };
 EventSaver eventSaver;
 
-#include <U8g2lib.h>
+#define USE_LASER_SENSOR
 
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-#ifdef U8X8_HAVE_HW_I2C
+#ifdef USE_LASER_SENSOR
+#include <ComponentObject.h>
+#include <RangeSensor.h>
+#include <SparkFun_VL53L1X.h>
+#include <vl53l1x_class.h>
+#include <vl53l1_error_codes.h>
+
+/*
+  Reading distance from the laser based VL53L1X
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: April 4th, 2018
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+*/
+
 #include <Wire.h>
-#endif
+#include "SparkFun_VL53L1X.h"
 
-U8G2_SSD1327_EA_W128128_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); /* Uno: A4=SDA, A5=SCL, add "u8g2.setBusClock(400000);" into setup() for speedup if possible */
+SFEVL53L1X distanceSensor;
 
-void u8g2_prepare(void) {
-  u8g2.setFont(u8g2_font_inb63_mn);
-  u8g2.setFontRefHeightExtendedText();
-  u8g2.setDrawColor(1);
-  u8g2.setFontPosTop();
-  u8g2.setFontDirection(0);
+void setup_distance_sensor() {
+  Wire.begin();
+  if (distanceSensor.begin() == 0) //Begin returns 0 on a good init
+  {
+    Serial.println("Laser distance sensor online!");
+  }
 }
 
-void drawUTF8(String val) {
-  u8g2.firstPage();
-  do {
-      u8g2_prepare();
-      u8g2.setFont(u8g2_font_ncenB14_tr);
-      u8g2.drawUTF8(0, 0, val.c_str());
-  } while( u8g2.nextPage() );
-}
+long calc_distance() {
+  distanceSensor.startRanging(); //Write configuration bytes to initiate measurement
+  while (!distanceSensor.checkForDataReady()) {
+    delay(1);
+  }
+  int distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
+  distanceSensor.clearInterrupt();
+  distanceSensor.stopRanging();
 
-void drawInt(int val) {
-  eventSaver.addEvent("drawInt");
-  u8g2.firstPage();
-  do {
-      u8g2_prepare();
-      u8g2.drawUTF8(30, 10, String(val).c_str());
-  } while( u8g2.nextPage() );
+  float distanceInches = distance * 0.0393701;
+  float distanceFeet = distanceInches / 12.0;
+  return (long)round(distanceFeet);
 }
-
-void setup_OLED() {
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  digitalWrite(10, 0);
-  digitalWrite(9, 0);
-  u8g2.begin();
-  u8g2.setBusClock(400000);
-}
+#else // USE_LASER_SENSOR
 
 const int trigPin = 6;
 const int echoPin = 7;
@@ -147,6 +146,53 @@ long calc_distance() {
 void setup_distance_sensor() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+}
+#endif // USE_LASER_SENSOR
+
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8G2_SSD1327_EA_W128128_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); /* Uno: A4=SDA, A5=SCL, add "u8g2.setBusClock(400000);" into setup() for speedup if possible */
+
+void u8g2_prepare(void) {
+  u8g2.setFont(u8g2_font_inb63_mn);
+  u8g2.setFontRefHeightExtendedText();
+  u8g2.setDrawColor(1);
+  u8g2.setFontPosTop();
+  u8g2.setFontDirection(0);
+}
+
+void drawUTF8(String val) {
+  u8g2.firstPage();
+  do {
+      u8g2_prepare();
+      u8g2.setFont(u8g2_font_ncenB14_tr);
+      u8g2.drawUTF8(0, 0, val.c_str());
+  } while( u8g2.nextPage() );
+}
+
+void drawInt(int val) {
+  eventSaver.addEvent("drawInt");
+  u8g2.firstPage();
+  do {
+      u8g2_prepare();
+      u8g2.drawUTF8(30, 10, String(val).c_str());
+  } while( u8g2.nextPage() );
+}
+
+void setup_OLED() {
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  digitalWrite(10, 0);
+  digitalWrite(9, 0);
+  u8g2.begin();
+  u8g2.setBusClock(400000);
 }
 
 #include <EEPROM.h>
